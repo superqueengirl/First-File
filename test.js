860c06432e118f37099e893932721e6a25acda8b
zy <1289015833@qq.com>
2016-10-29 11:15:18 +0800

    tijiao

--- test.js
+++ test.js
@@ -2,9 +2,10 @@
 * @Author: anchen
 * @Date:   2016-10-29 10:50:44
 * @Last Modified by:   anchen
-* @Last Modified time: 2016-10-29 11:09:57
+* @Last Modified time: 2016-10-29 11:14:04
 */
 
 'use strict';
 console.log('哈哈');
 alert('恩恩');
+console.log('吼吼');
