2f9f6ae07521ec57af7eed3a304efb1ebf83bb18
zy <1289015833@qq.com>
2016-10-29 11:19:07 +0800

    file

--- test.js
+++ test.js
@@ -2,8 +2,7 @@
 * @Author: anchen
 * @Date:   2016-10-29 10:50:44
 * @Last Modified by:   anchen
-* @Last Modified time: 2016-10-29 10:56:49
+* @Last Modified time: 2016-10-29 11:18:37
 */
 
 'use strict';
-console.log('哈哈');
